432de4f521663f2f9119b64fe34918c0b3ea7b53
iamtatendakagande <ttkagande@gmail.com>
2024-02-28 21:13:32 +0200

    1

--- static/js/irent.js
+++ static/js/irent.js
@@ -2,7 +2,15 @@
     'use strict';
 
 jQuery(document).ready(function () {
+  const dismissablePanels = document.querySelectorAll('.dismissable-panel');
 		
+   dismissablePanels.forEach(panel => {
+      const closeButton = panel.querySelector('.dismiss-button');
+      closeButton.addEventListener('click', () => {
+        panel.style.display = 'none';
+      });
+    });
+
 
 });
 })(jQuery);
